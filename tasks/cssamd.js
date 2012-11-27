ad0ba9e40f810cf9ee23d1d09ef334f3bfa4d1d4
thiagofelix <felixthi@gmail.com>
2012-11-27 02:24:08 -0200

    doing some correction on module template code

--- tasks/cssamd.js
+++ tasks/cssamd.js
@@ -23,26 +23,30 @@ module.exports = function(grunt) {
     var srcFiles;
     var sourceCss;
     var sourceModuled;
-    var outputFilename; 
-
+    var fileName; 
+    var outputFilename;
+    var moduleName;
+    
     this.files.forEach(function(files) {      
 
       srcFiles = grunt.file.expandFiles(files.src);
 
       srcFiles.forEach(function(srcFile) {        
         grunt.log.writeln('Processing "' + srcFile + ".");
-        try{          
+
+        fileName = srcFile.substr(srcFile.lastIndexOf('/')+1);
+
+        try{                      
+          moduleName = options.moduleNamePrefix || files.dest + fileName;   
           sourceCss = grunt.file.read(srcFile);
-          sourceModuled = modulyCSS(outputFilename, sourceCss);          
+          sourceModuled = modulyCSS(moduleName, sourceCss);          
         } catch(e) {
           grunt.log.error(e);
           grunt.fail.warn('Failed to compile css '+srcFile+'.'); 
         }
 
-        try{
-          //outputFilename = target directory + cssFileName + .js
-          outputFilename = files.dest + srcFile.substr(srcFile.lastIndexOf('/')+1) + '.js';
-          
+        try{          
+          outputFilename = files.dest + fileName + '.js';
           grunt.file.write(outputFilename, sourceModuled);
           grunt.log.writeln('File "' + outputFilename + '" created.');
         } catch(e) {
@@ -61,8 +65,8 @@ module.exports = function(grunt) {
       output += "define('"+moduleName+"',[], function(){\n";
       output += "\tvar style = document.createElement('style');\n";
       output += "\tstyle.appendChild(\n";
-      output += "\t\tdocument.createTextNode('"+source.replace(/[\r\n]/g,"")+"');\n";
-      output += "\t});\n";
+      output += "\t\tdocument.createTextNode('"+source.replace(/[\r\n]/g,"")+"')\n";
+      output += "\t);\n";
       output += "\treturn style;\n";
       output += "});\n";
       return output;
--- test/expected/basic.css.js
+++ test/expected/basic.css.js
@@ -1,7 +1,7 @@
-define('undefined',[], function(){
+define('tmp/modules/basic.css',[], function(){
 	var style = document.createElement('style');
 	style.appendChild(
-		document.createTextNode('.myView {  padding: 5px;  color: #f0f;}.myView__foo {  border: 1px solid #0f0;}');
-	});
+		document.createTextNode('.myView {  padding: 5px;  color: #f0f;}.myView__foo {  border: 1px solid #0f0;}')
+	);
 	return style;
 });
