76ec241309adf5b46f7386c8a4f06e1fab3d335a
Troels Johnsen <troelsjohnsen@gmail.com>
2013-09-19 17:53:20 +0200

    Update cssamd.js
    Fixed to work in Grunt 0.4

--- tasks/cssamd.js
+++ tasks/cssamd.js
@@ -7,62 +7,53 @@
  * https://github.com/thiagofelix/grunt-css-amd/blob/master/LICENSE-MIT
  */
 
+"use strict";
+
 module.exports = function(grunt) {
-  "use strict";
 
   grunt.registerMultiTask("cssamd", "Compile all css files into javascript amd module files", function() {
+    grunt.log.writeln('Yeah, we are rocking! ', this.files.length);
 
-    var helpers = require('grunt-lib-contrib').init(grunt);
-    var options = helpers.options(this);
+    if (this.files.length < 1) {
+      grunt.verbose.warn('Destination not written because no source files were provided.');
+    }
 
-    grunt.verbose.writeflags(options, "Options");
-    
-    // TODO: ditch this when grunt v0.4 is released
-    this.files = this.files || helpers.normalizeMultiTaskFiles(this.data, this.target);
-
-    var srcFiles;
-    var sourceCss;
-    var sourceModuled;
-    var fileName; 
-    var outputFilename;
-    var moduleName;
-    
-    this.files.forEach(function(files) {      
-
-      srcFiles = grunt.file.expandFiles(files.src);
-
-      srcFiles.forEach(function(srcFile) {        
-        grunt.log.writeln('Processing "' + srcFile + ".");
-
-        fileName = srcFile.substr(srcFile.lastIndexOf('/')+1);
-
-        try{                      
-          moduleName = options.moduleNamePrefix || files.dest + fileName;   
-          sourceCss = grunt.file.read(srcFile);
-          sourceModuled = modulyCSS(moduleName, sourceCss);          
-        } catch(e) {
-          grunt.log.error(e);
-          grunt.fail.warn('Failed to compile css '+srcFile+'.'); 
-        }
-
-        try{          
-          outputFilename = files.dest + fileName + '.js';
-          grunt.file.write(outputFilename, sourceModuled);
-          grunt.log.writeln('File "' + outputFilename + '" created.');
-        } catch(e) {
-          grunt.log.error(e);
-          grunt.fail.warn('Failed to save transformed css '+outputFilename+'.'); 
+    this.files.forEach(function(f) {
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
         }
       });
-    });    
+
+      if (src.length === 0) {
+        grunt.log.warn('Destination (' + f.dest + ') not written because src files were empty.');
+        return;
+      }
+
+      var css = grunt.file.read(src);
+
+      var output = CSS2Module(css);
+
+      // Write the destination file.
+      grunt.file.write(f.dest, output);            
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+
+    });
+
   });
-  
-  var modulyCSS = function(moduleName, source) {
+
+  var CSS2Module = function(source) {
     try {
       
       var output  = "";
 
-      output += "define('"+moduleName+"',[], function(){\n";
+      output += "define([], function(){\n";
       output += "\tvar style = document.createElement('style');\n";
       output += "\tstyle.appendChild(\n";
       output += "\t\tdocument.createTextNode('"+source.replace(/[\r\n]/g,"")+"')\n";
@@ -75,5 +66,4 @@ module.exports = function(grunt) {
       grunt.fail.warn('CSS modularization failed.');
     }
   };
-
 };
